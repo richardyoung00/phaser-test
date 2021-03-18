38789d9f0b20dfabda5e35aabe37551d72bb3845
Richard Young <richard.young2@ibm.com>
2021-03-18 12:23:41 +0200

    Use vite

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -1,16 +1,20 @@
 import Phaser from 'phaser'
 
+import sokoban_tilesheet from '../../public/textures/sokoban_tilesheet.png'
+import character from '../../public/textures/characters.png'
+import concrete from '../../public/textures/concrete.jpg'
+
 export default class Preloader extends Phaser.Scene {
     constructor() {
         super('preloader')
     }
 
     preload() {
-        this.load.spritesheet('sokoban', 'textures/sokoban_tilesheet.png', {
+        this.load.spritesheet('sokoban', sokoban_tilesheet , {
             frameWidth: 64
         })
-        this.load.image('character', 'textures/characters.png');
-        this.load.image('concrete', 'textures/concrete.jpg');
+        this.load.image('character', character);
+        this.load.image('concrete', concrete);
     }
 
     create() {
